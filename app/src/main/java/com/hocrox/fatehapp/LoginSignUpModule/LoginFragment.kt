2812a53b16eff7a0243d165876e8dbb21f617aa6
sahil <sahilgoyal9994@gmail.com>
2017-08-17 21:04:55 +0530

    first commit

---
+++ app/src/main/java/com/hocrox/fatehapp/LoginSignUpModule/LoginFragment.kt
@@ -0,0 +1,12 @@
+package com.hocrox.fatehapp.LoginSignUpModule
+
+import android.support.v4.app.Fragment
+
+/**
+ * Created by sahilgoyal on 17/8/17.
+ */
+public class LoginFragment:Fragment(){
+
+
+
+}
---
+++ app/src/main/java/com/hocrox/fatehapp/LoginSignUpModule/LoginSignUpActivity.kt
@@ -0,0 +1,13 @@
+package com.hocrox.fatehapp.LoginSignUpModule
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import com.hocrox.fatehapp.R
+
+class LoginSignUpActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login_sign_up)
+    }
+}
